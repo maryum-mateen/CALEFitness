31ab7235349d6edc259933c933e855cb5294e4f5
Maryum Mateen <maryum.mateen@credera.com>
2023-06-06 14:36:34 -0500

    Initial Commit

---
+++ CALEFitness/CALEFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  CALEFitnessApp.swift
+//  CALEFitness
+//
+//  Created by Maryum Mateen on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CALEFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
